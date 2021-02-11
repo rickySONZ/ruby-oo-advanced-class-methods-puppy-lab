96d8264344d4ca94e39839b6a8c954550aa9a9ad
Ryan Erricson <ryanerricson@gmail.com>
2021-02-11 09:04:16 -0500

    commit tree b3c430fdf907c357ef4c80a1c5a3e210aa181376

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
 # Add your code here
+
+
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        @@all.each do |d|
+            puts d.name
+        end
+    end
+
+    def save
+        @@all << self
+    end
+end
